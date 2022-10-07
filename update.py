1c8c2d7134f46c85ff2d73747676050c72643ffe
alokhj83077 <113902349+alokhj83077@users.noreply.github.com>
2022-10-07 20:04:53 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/Reflection-Mirror/Reflection-Mirror"
+    UPSTREAM_REPO = "https://github.com/alokhj83077/dipesbot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
